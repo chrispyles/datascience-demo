73f3bb2a6d87e68ebf8399505877dd2fd1c88e9a
Chris Pyles <cpyles@berkeley.edu>
2019-06-25 16:31:18 -0700

    fixed q2 test

--- tests/q2.py
+++ tests/q2.py
@@ -7,6 +7,7 @@ test = {
         {
           'code': r"""
           >>> "Duration^2" in trips.labels
+          True
           >>> trips.where("Trip ID", 875617)["Duration^2"][0]
           525625
           """,
